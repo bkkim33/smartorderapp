85866024f53c71119abd20b8d52207bf694f2667
bkkim33 <bkkim@mz.co.kr>
2023-09-19 17:28:21 +0900

    app 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./page/MainPage";
 import Login from "./page/LoginPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <BrowserRouter basename="process.env.https://bkkim33.github.io/smartorderapp/">
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
