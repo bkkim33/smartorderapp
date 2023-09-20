90ead253dc2e0f9f545d0454ac4cfcfb3173c662
bkkim33 <bkkim@mz.co.kr>
2023-09-20 11:15:00 +0900

    앱수정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Main />} />
-      <Route exact path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
