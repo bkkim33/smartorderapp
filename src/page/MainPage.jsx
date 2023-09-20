52a12e2301181df897ad328abdb7c472395bffd3
bkkim33 <bkkim@mz.co.kr>
2023-09-20 17:58:52 +0900

    버튼 만드는중~

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from '../layout/DefaultLayout'
+import Button from '../components/Button'
 
 function MainPage() {
   return (
     <Layout>
       <div>main</div>
       <Link to="/login">로그인 이동</Link>
+      <Button name='mt-10'>
+        ssssssss
+      </Button>
     </Layout>
   );
 }
