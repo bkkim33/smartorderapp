a366b6b94bb8ef72a97b5e4b84b5779e3cab5ba6
bkkim33 <bkkim@mz.co.kr>
2023-09-22 17:43:17 +0900

    글로벌 css 추가

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import "./styles/common/reset.scss";
+import "./styles/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
