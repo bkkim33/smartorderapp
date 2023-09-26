623d0aab4849e1dbd65932bc921d2b63638aad68
bkkim33 <bkkim@mz.co.kr>
2023-09-26 16:27:27 +0900

    커밋

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Layout from '../layout/DefaultLayout'
 import Button from '../components/Button'
+import { Header } from "../stories/Header";
+
+
 
 function MainPage() {
   return (
     <Layout>
       <div>main</div>
       <Link to="/login">로그인 이동</Link>
-      <Button name='mt-10'>
-        ssssssss
-      </Button>
+      <Button name="mt-10">ssssssss</Button>
+      <Header
+        onCreateAccount={() => {}}
+        onLogin={() => {}}
+        onLogout={() => {}}
+        user={{
+          name: "Jane Doe",
+        }}
+      />
     </Layout>
   );
 }
