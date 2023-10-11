95b0f5aeaa7f125cca0567d1c44a289a0e9e3a61
bkkim33 <bkkim@mz.co.kr>
2023-10-11 17:05:28 +0900

    셀렉트 디스만 남음

---
+++ src/stories/components/Select.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Select from "../../components/Select";
+
+export default {
+  title: "components/Select",
+  component: Select,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+export const Default = (args) => (
+  <div style={{ width: "100%", height: "250px" }}>
+    {/* div는 영역을 넓히기 위해 스토리북에만 사용하는 태그입니다. 해당 태그를
+    제외하고 사용하세요. */}
+    <Select {...args}>{args.children}</Select>
+  </div>
+);
+
+export const DefaultValue = () => (
+  <div style={{ width: "100%", height: "250px" }}>
+    {/* div는 영역을 넓히기 위해 스토리북에만 사용하는 태그입니다. 해당 태그를
+    제외하고 사용하세요. */}
+    <Select defaultValue={options[1]}></Select>
+  </div>
+);
+
