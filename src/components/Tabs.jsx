994d89abe1d2c300f2fa17d0938ac30f2ab33a3f
MZ-Leejinhee <92710726+MZ-Leejinhee@users.noreply.github.com>
2023-10-20 18:24:46 +0900

    tab 완성

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
+import styles from "../styles/components/tabs.module.scss"
+
+Tabs.propTypes = {
+    type: PropTypes.oneOf(["","round"]),
+    globalClass: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+Tabs.defaultProps = {
+    type: "",
+    globalClass: "",
+};
+
+export function Tabs({ TabsBtn, onClick, ...others }){
+    const { type, globalClass } = others;
+    const [active, setActive] = useState(null);
+    return (
+        <div className={` ${styles.tabs} ${styles[type]} ${globalClass || ''} `}>
+            {
+              TabsBtn.map((btn) => (
+                <button 
+                  className={`${active === btn ? styles.active : ""}`} 
+                  onClick={() => setActive(btn)} 
+                  key={btn.id}
+                >
+                  {btn.title}
+                </button>
+              ))
+            }
+        </div>
+    );
+};
+
+export default Tabs;
