204cbc5e550b9128991f97cc449bff28d7685ba7
bkkim33 <bkkim@mz.co.kr>
2023-10-25 12:45:11 +0900

    작업 중

--- src/page/app/MainPage.jsx
+++ src/page/app/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from '../../layout/AppDefaultLayout'
 import Mainimg from "../../images/main_img.gif";
 
@@ -6,7 +6,78 @@ import FullMenu from "./FullMenuPage";
 
 import Tabs from "../../components/Tabs";
 
-// import { Icons } from "../../components/Icon";
+import { Icons } from "../../components/Icon";
+
+const LocationsData = [
+  {
+    id: 1,
+    title: "클라우드 카페",
+    location: "서울 강남구 논현로 85길 46 메가존빌딩 1F",
+    time: "매일 08:00~22:00",
+  },
+  {
+    id: 2,
+    title: "클라우드 카페",
+    location: "서울 강남구 논현로 85길 46 메가존빌딩 1F",
+    time: "매일 08:00~22:00",
+  },
+  {
+    id: 3,
+    title: "클라우드 카페",
+    location: "서울 강남구 논현로 85길 46 메가존빌딩 1F",
+    time: "매일 08:00~22:00",
+  },
+];
+
+function Locations() {
+  const [open, setOpen] = useState(false);
+  const [titleSelect, setTitleSelect] = useState();
+  const LocationonCLick = () => {
+    setOpen(!open);
+  };
+  const ListCLick = (e) => {
+    setOpen(!open);
+    setTitleSelect(e.target.LocationsData.title);
+  };
+  return (
+    <div className="main_locations">
+      <button onClick={LocationonCLick}>
+        <p className="headline1">
+          <span>{titleSelect}</span>
+          <span>선택해 주세요!</span>
+        </p>
+
+        <i className={`${open ? "open" : ""}`}>
+          <Icons.ArrowBg
+            width={36}
+            height={36}
+            fill="var(--blue)"
+            stroke="#DAEE5F"
+          />
+        </i>
+      </button>
+      <div className={`${"main_locations_list"} ${open ? "open" : ""}`}>
+        <ul>
+          {LocationsData.map((location) => (
+            <li key={location.id}>
+              <button onClick={ListCLick}>
+                <i>
+                  <Icons.LogoOnly width={20} height={20} />
+                </i>
+                <p>
+                  <span>{location.title}</span>
+                  <span>{location.location}</span>
+                  <span>{location.time}</span>
+                </p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+ 
+}
 
 function MainPage() {
   return (
@@ -32,15 +103,7 @@ function MainPage() {
           </div>
         </div>
         <div className="main_content">
-          <div className="main_locations">
-            <button>
-              <p>
-                <span>주문매장을</span>
-                <span>선택해 주세요</span>
-              </p>
-
-            </button>
-          </div>
+          <Locations />
           <Tabs
             TabsData={[
               {
@@ -64,8 +127,7 @@ function MainPage() {
               },
             ]}
             onClick={() => {}}
-          ></Tabs>
-          <div style={{ height: "100%" }}>main</div>
+          />
         </div>
       </div>
     </Layout>
