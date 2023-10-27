6280245a4ae78bba69c7a5c70e4234236859d514
bkkim33 <bkkim@mz.co.kr>
2023-10-27 09:52:58 +0900

    매뉴 딤드 구성 변경

--- src/layout/AppSideMenuLayout.jsx
+++ src/layout/AppSideMenuLayout.jsx
@@ -22,80 +22,83 @@ function SideMenuLayout({ globalstyle, onClose, open }) {
   // }, []);
 
   return (
-    <aside
-      className={`${styles.menu} ${open ? styles.open : ""} ${
-        globalstyle | ""
-      }`}
-    >
-      <nav>
-        <div className={`${styles.menu_top}`}>
-          <div className={`${styles.menu_header}`}>
-            <Button icon="Home" none onClick={() => {}} size="icon_s">
-              button
-            </Button>
-            <Button icon="Delete" none onClick={onClose} size="icon_s">
-              닫기
+    <>
+      <aside
+        className={`${styles.menu} ${open ? styles.open : ""} ${
+          globalstyle | ""
+        }`}
+      >
+        <nav>
+          <div className={`${styles.menu_top}`}>
+            <div className={`${styles.menu_header}`}>
+              <Button icon="Home" none onClick={() => {}} size="icon_s">
+                button
+              </Button>
+              <Button icon="Delete" none onClick={onClose} size="icon_s">
+                닫기
+              </Button>
+            </div>
+            <div className={`${"align mt_30"} ${styles.menu_user}`}>
+              <div className={`${"lft"} ${styles.menu_user_txt}`}>
+                <p>
+                  <span>
+                    <strong className="neon">김이준</strong> 님,
+                  </span>
+                  <span>좋은 하루 보내세요!</span>
+                </p>
+              </div>
+              <div className={`${"rgt"} ${styles.menu_user_img}`}>
+                <img src={Userimg} alt="유저 이미지" />
+              </div>
+            </div>
+            <ul className={`${styles.menu_wrap} `}>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.MenuCoupon width={16} height={16} fill="#3A3A3A" />
+                  <span>쿠폰함</span>
+                </Link>
+              </li>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.MenuOrder width={16} height={16} fill="#3A3A3A" />
+                  <span>주문내역</span>
+                </Link>
+              </li>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.MenuPrivacy width={16} height={16} fill="#3A3A3A" />
+                  <span>내 정보</span>
+                </Link>
+              </li>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.MenuNotice width={16} height={16} fill="#3A3A3A" />
+                  <span>공지사항</span>
+                </Link>
+              </li>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.Question width={16} height={16} fill="#3A3A3A" />
+                  <span>자주하는 질문</span>
+                </Link>
+              </li>
+              <li className={`${styles.menu_dep1}`}>
+                <Link to="/admin/login">
+                  <Icons.Infomation width={16} height={16} fill="#3A3A3A" />
+                  <span>이용약관</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={`${styles.menu_bottom}`}>
+            <Button onClick={() => {}} size="full">
+              로그아웃
             </Button>
           </div>
-          <div className={`${"align mt_30"} ${styles.menu_user}`}>
-            <div className={`${"lft"} ${styles.menu_user_txt}`}>
-              <p>
-                <span>
-                  <strong className="neon">김이준</strong> 님,
-                </span>
-                <span>좋은 하루 보내세요!</span>
-              </p>
-            </div>
-            <div className={`${"rgt"} ${styles.menu_user_img}`}>
-              <img src={Userimg} alt="유저 이미지" />
-            </div>
-          </div>
-          <ul className={`${styles.menu_wrap} `}>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.MenuCoupon width={16} height={16} fill="#3A3A3A" />
-                <span>쿠폰함</span>
-              </Link>
-            </li>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.MenuOrder width={16} height={16} fill="#3A3A3A" />
-                <span>주문내역</span>
-              </Link>
-            </li>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.MenuPrivacy width={16} height={16} fill="#3A3A3A" />
-                <span>내 정보</span>
-              </Link>
-            </li>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.MenuNotice width={16} height={16} fill="#3A3A3A" />
-                <span>공지사항</span>
-              </Link>
-            </li>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.Question width={16} height={16} fill="#3A3A3A" />
-                <span>자주하는 질문</span>
-              </Link>
-            </li>
-            <li className={`${styles.menu_dep1}`}>
-              <Link to="/admin/login">
-                <Icons.Infomation width={16} height={16} fill="#3A3A3A" />
-                <span>이용약관</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className={`${styles.menu_bottom}`}>
-          <Button onClick={() => {}} size="full">
-            로그아웃
-          </Button>
-        </div>
-      </nav>
-    </aside>
+        </nav>
+      </aside>
+      <div className={`${styles.dimd} ${open ? styles.open : ""}`}></div>
+    </>
   );
 }
 
