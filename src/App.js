6e1827d67b5546bb2c53b4828aade82d659dad32
bkkim33 <bkkim@mz.co.kr>
2023-11-21 10:42:24 +0900

    앱 수정중

--- src/App.js
+++ src/App.js
@@ -56,47 +56,91 @@ import OrderComplete from "./page/app/ordercomplete/OrderComplete";
 function App() {
   return (
     <Routes>
-      {/* APP */}
+      {/* APP : 각 페이지에 레이어 팝업이 있을시 페이지 내부에 존재합니다. */}
       <Route>
+        {/* APP로그인 */}
         <Route exact path="/" element={<AppLogin />} />
+        {/* APP QR접근 로그인 */}
         <Route path="/qr" element={<AppQRLogin />} />
+        {/* APP 메인 */}
         <Route path="/main" element={<AppMain />} />
+        {/* APP QR접근 메인 */}
         <Route path="/qrmain" element={<QRAppMain />} />
+        {/* APP 메뉴 상세 */}
         <Route path="/order" element={<MenuDetail />} />
+        {/* APP 메뉴 QR접근 상세 */}
         <Route path="/qrorder" element={<MenuDetail QR />} />
+        {/* APP 주문내역 */}
         <Route path="/order/history" element={<OrderHistory />} />
+        {/* APP 장바구니 */}
         <Route path="/cart" element={<CartPage />} />
+        {/* APP 구매(결제) */}
         <Route path="/payment" element={<Payment />} />
-        <Route path="/coupon" element={<Coupon />} />
-        <Route path="/notice" element={<NoticeList />} />
-        <Route path="/notice/detail" element={<NoticeDetail />} />
-        <Route path="/terms" element={<TermsList />} />
-        <Route path="/terms/detail" element={<TermsDetail />} />
-        <Route path="/myinfo" element={<MyInfo />} />
-        <Route path="/faq" element={<Faq />} />
+        {/* APP 결제 완료 */}
         <Route path="/complete" element={<OrderComplete />} />
+        {/* APP 쿠폰 */}
+        <Route path="/coupon" element={<Coupon />} />
+        {/* APP 공지사항 리스트 */}
+        <Route path="/notice" element={<NoticeList />} />
+        {/* APP 공지사항 상세 */}
+        <Route path="/notice/detail" element={<NoticeDetail />} />
+        {/* APP 약관 리스트 */}
+        <Route path="/terms" element={<TermsList />} />
+        {/* APP 약관 상세 */}
+        <Route path="/terms/detail" element={<TermsDetail />} />
+        {/* APP 내 정보 */}
+        <Route path="/myinfo" element={<MyInfo />} />
+        {/* APP 자주하는 질문 */}
+        <Route path="/faq" element={<Faq />} />
       </Route>
 
-      {/* <Route path="/app/login" element={<Login />} />c */}
-      {/* ADMIN */}
+      {/* ADMIN : 각 페이지에 레이어 팝업이 있을시 페이지 내부에 존재합니다. */}
       <Route>
+        {/* ADMIN 로그인 */}
         <Route path="/admin" element={<AdminLogin />} />
+        {/* ADMIN 상품 전시 관리 - 상품관리 */}
         <Route path="/admin/product/list" element={<ProductList />} />
-        <Route path="/admin/product/registration" element={<ProductRegistrationPage />} />
-        <Route path="/admin/product/category"  element={<CategoryManagementPage />} />
+        {/* ADMIN 상품 전시 관리 - 상품정보등록 */}
+        <Route
+          path="/admin/product/registration"
+          element={<ProductRegistrationPage />}
+        />
+        {/* ADMIN 상품 전시 관리 - 카테고리 관리 */}
+        <Route
+          path="/admin/product/category"
+          element={<CategoryManagementPage />}
+        />
         <Route path="/admin/order/detail" element={<OrderDetail />} />
         <Route path="/admin/customer/notice" element={<AdminNotice />} />
         {/* 진현주 - 등록페이지 추가 */}
-        <Route path="/admin/customer/notice/registration"  element={<NoticeRegistrationPage/>} />
-        <Route path="/admin/customer/qna/registration"  element={<QnaRegistrationPage/>} />
-        <Route path="/admin/customer/terms/registration"  element={<TermsRegistrationPage/>} />
-        <Route path="/admin/store/info/registration"  element={<StoreInfoRegistrationPage/>} />
+        <Route
+          path="/admin/customer/notice/registration"
+          element={<NoticeRegistrationPage />}
+        />
+        <Route
+          path="/admin/customer/qna/registration"
+          element={<QnaRegistrationPage />}
+        />
+        <Route
+          path="/admin/customer/terms/registration"
+          element={<TermsRegistrationPage />}
+        />
+        <Route
+          path="/admin/store/info/registration"
+          element={<StoreInfoRegistrationPage />}
+        />
         {/* 진현주 - 매장정보 관리 페이지 추가  */}
-        <Route path="/admin/store/info"  element={<StoreInfoPage/>} />
+        <Route path="/admin/store/info" element={<StoreInfoPage />} />
         {/* 진현주 - 매장정보 수정 페이지 추가  */}
-        <Route path="/admin/store/info/modify"  element={<StoreInfoModifyePage/>} />
+        <Route
+          path="/admin/store/info/modify"
+          element={<StoreInfoModifyePage />}
+        />
         {/* 공지사항 수정 */}
-        <Route path="/admin/customer/notice/modify"  element={<NoticeModifyPage/>} />
+        <Route
+          path="/admin/customer/notice/modify"
+          element={<NoticeModifyPage />}
+        />
 
         <Route path="/admin/user" element={<UserList />} />
         <Route path="/admin/account" element={<AccountList />} />
@@ -104,8 +148,10 @@ function App() {
         <Route path="/admin/account/modify" element={<AccountModify />} />
         {/* 진현주 - 내 프로필 페이지 추가 */}
         <Route path="/admin/myprofile/profile" element={<Profile />} />
-        <Route path="/admin/myprofile/changepassword"  element={<ChangePassword />} />
-
+        <Route
+          path="/admin/myprofile/changepassword"
+          element={<ChangePassword />}
+        />
       </Route>
       {/* KDS */}
       {/* <Route path="/*" element={<Main />} /> */}
