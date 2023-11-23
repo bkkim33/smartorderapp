d864b37858a4574bd116002b0c48ac8c4b9314e6
bkkim33 <bkkim@mz.co.kr>
2023-11-23 10:25:57 +0900

    형태 수정

--- src/page/admin/store/register/PickupMethod.jsx
+++ src/page/admin/store/register/PickupMethod.jsx
@@ -8,6 +8,7 @@ import Select from "../../../../components/Select";
 
 
 //mui table import
+import MuiAlert from "../../../../components/MuiAlert";
 import MuiTable from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -21,6 +22,14 @@ const inUse = [
 ];
 
 function PickupMethod() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const [receiptMethodsData, setReceiptMethodsData] = useState([
     { id: 1, name: "매장픽업", selectedOption: "Y" },
     { id: 2, name: "테이블 수령", selectedOption: "Y" },
@@ -36,75 +45,137 @@ function PickupMethod() {
   };
 
   return (
-    <div className="admin_receivingmethod">
-      <div className="align mb_20">
-        {/* <h2 className="headline4">수령방법 등록</h2> */}
-        <div className="btn_set">
-          <Button icon="AdminArrow" onClick={() => {}} size="icon_s">
-            위로
-          </Button>
-          <Button icon="AdminArrow" onClick={() => {}} size="icon_s">
-            아래로
-          </Button>
-          <Button icon="AdminArrowBar" onClick={() => {}} size="icon_s">
-            한 페이지 위로
-          </Button>
-          <Button icon="AdminArrowBar" onClick={() => {}} size="icon_s">
-            한 페이지 아래로
+    <div className="admin_receivingmethod align column">
+      <div className=" align column flex1">
+        <div className="align mb_20">
+          {/* <h2 className="headline4">수령방법 등록</h2> */}
+          <div className="btn_set">
+            <Button icon="AdminArrow" onClick={() => {}} size="icon_s">
+              위로
+            </Button>
+            <Button icon="AdminArrow" onClick={() => {}} size="icon_s">
+              아래로
+            </Button>
+            <Button icon="AdminArrowBar" onClick={() => {}} size="icon_s">
+              한 페이지 위로
+            </Button>
+            <Button icon="AdminArrowBar" onClick={() => {}} size="icon_s">
+              한 페이지 아래로
+            </Button>
+          </div>
+          <Button onClick={() => {}} size="small_h35" border="point">
+            추가
           </Button>
         </div>
-        <Button onClick={() => {}} size="small_h35" border="point">
-          추가
+        <div className="tbl">
+          <TableContainer>
+            <MuiTable>
+              <colgroup>
+                <col width="10%" />
+                <col width="auto" />
+                <col width="auto" />
+                <col width="30px" />
+              </colgroup>
+              <TableHead>
+                <TableRow>
+                  <TableCell>순서</TableCell>
+                  <TableCell>수령방법</TableCell>
+                  <TableCell>사용 여부</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>1</TableCell>
+                  <TableCell>
+                    <Input onClick={() => {}} />
+                  </TableCell>
+                  <TableCell>
+                    <Select
+                      minwidth="100%"
+                      options={inUse}
+                      defaultValue={0}
+                      value={receiptMethodsData.selectedOption}
+                      onChange={(e) =>
+                        handleSelectChange(
+                          receiptMethodsData.id,
+                          e.target.value
+                        )
+                      }
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      globalClass="rgt"
+                      icon="Delete"
+                      line={{}}
+                      onClick={() => {}}
+                      size="icon_s"
+                    />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>2</TableCell>
+                  <TableCell>
+                    <Input onClick={() => {}} />
+                  </TableCell>
+                  <TableCell>
+                    <Select
+                      minwidth="100%"
+                      options={inUse}
+                      defaultValue={0}
+                      value={receiptMethodsData.selectedOption}
+                      onChange={(e) =>
+                        handleSelectChange(
+                          receiptMethodsData.id,
+                          e.target.value
+                        )
+                      }
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      globalClass="rgt"
+                      icon="Delete"
+                      line={{}}
+                      onClick={() => {}}
+                      size="icon_s"
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </MuiTable>
+          </TableContainer>
+        </div>
+      </div>
+      <div className="align end gap_10">
+        <Button line size="xlarge">
+          목록
+        </Button>
+        <Button onClick={handleOpen} btntype="c11" size="xlarge">
+          저장
         </Button>
       </div>
-      <div className="tbl">
-        <TableContainer>
-          <MuiTable>
-            <colgroup>
-              <col width="10%" />
-              <col width="auto" />
-              <col width="auto" />
-              <col width="30px" />
-            </colgroup>
-            <TableHead>
-              <TableRow>
-                <TableCell>순서</TableCell>
-                <TableCell>수령방법</TableCell>
-                <TableCell>사용 여부</TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>1</TableCell>
-                <TableCell>
-                  <Input onClick={() => {}} />
-                </TableCell>
-                <TableCell>
-                  <Select
-                    minwidth="100%"
-                    options={inUse}
-                    defaultValue={0}
-                    value={receiptMethodsData.selectedOption}
-                    onChange={(e) =>
-                      handleSelectChange(receiptMethodsData.id, e.target.value)
-                    }
-                  />
-                </TableCell>
-                <TableCell>
-                  <Button
-                    globalClass="rgt"
-                    icon="Plus"
-                    line={{}}
-                    onClick={() => {}}
-                    size="icon_s"
-                  />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </MuiTable>
-        </TableContainer>
-      </div>
+      <MuiAlert
+        open={open}
+        onClose={handleClose}
+        title={
+          <>
+            입력한 내용을 <br />
+            저장하시겠습니까?
+          </>
+        }
+        button={
+          <>
+            <Button onClick={handleClose} line>
+              취소
+            </Button>
+            <Button onClick={() => {}} border="point">
+              확인
+            </Button>
+          </>
+        }
+      />
       {/* <div className="align column rgt">
         <div className="align mb_20">
           <h2 className="headline4">수령방법 설정</h2>
