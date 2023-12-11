0b0cf1b6fb2dea36a3ae571f9824dee060e42027
MZ-Leejinhee <92710726+MZ-Leejinhee@users.noreply.github.com>
2023-12-11 13:18:50 +0900

    날짜 선택 select width 수정

--- src/page/admin/customer/TermsListPage.jsx
+++ src/page/admin/customer/TermsListPage.jsx
@@ -70,7 +70,7 @@ function TermsListPage() {
             options={displayOpt} 
           />
         </div>
-        <div style={{ maxWidth: '250px' }}>
+        <div style={{ width: '250px' }}>
           <DatePicker
             dateFormat="yyyy/MM/dd"
             selected={startDate}
