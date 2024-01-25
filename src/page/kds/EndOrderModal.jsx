1a7ff1eb6bf109cb9cddfbe6626628a1eb9869cf
bkkim33 <bkkim@mz.co.kr>
2024-01-25 16:37:05 +0900

    팝업 스타일 작업 끝

--- src/page/kds/EndOrderModal.jsx
+++ src/page/kds/EndOrderModal.jsx
@@ -40,13 +40,13 @@ const EndOrderModal = () => {
   return (
     <div className="kds_modal">
       <div className="kds_modal_box">
-        <div className="kds_modal_pick">
+        {/* <div className="kds_modal_pick">
           <div className="kds_modal_top">
-            <div className="kds_modal_tit">
+            <div className="kds_modal_tit mt_20">
               <h3>픽업대 선택</h3>
             </div>
           </div>
-          <div className="kds_modal_picknum">
+          <div className="kds_modal_picknum" draggable>
             <button>
               <p>110</p>
             </button>
@@ -88,14 +88,63 @@ const EndOrderModal = () => {
               <li>
                 <span>8</span>
               </li>
-              
             </ul>
           </div>
+          <div className="kds_modal_btn">
+            <button>닫기</button>
+            <button>확인</button>
+          </div>
+        </div> */}
+        <div className="kds_modal_pick">
+          <div className="kds_modal_top">
+            <div className="kds_modal_tit">
+              <h3>서빙 선택</h3>
+            </div>
+          </div>
+          <div className="kds_modal_tab">
+            <div className="kds_modal_tab_lft">
+              <button className="active">로봇1</button>
+              <button>로봇2</button>
+              <button className="disabled">로봇3</button>
+              <button>직원 서빙</button>
+            </div>
+            <div className="kds_modal_tab_rgt">
+              <button className="active">트레이1</button>
+              <button>트레이2</button>
+              <button>트레이3</button>
+            </div>
+          </div>
+          <div className="kds_modal_btn">
+            <button>닫기</button>
+            <button>확인</button>
+          </div>
         </div>
-        <div className="kds_modal_btn">
-          <button>닫기</button>
-          <button>확인</button>
-        </div>
+        {/* <div className="kds_modal_pick">
+          <div className="kds_modal_top">
+            <div className="kds_modal_tit">
+              <h3>배달 선택</h3>
+            </div>
+          </div>
+          <div className="kds_modal_tab mt_65">
+            <div className="kds_modal_tab_lft">
+              <button>로봇1</button>
+              <button>로봇2</button>
+              <button className="disabled">로봇3</button>
+              <button className="active">직원 서빙</button>
+            </div>
+            <div className="kds_modal_tab_rgt">
+              <button className="active">양땡이</button>
+              <button>박땡이</button>
+              <button>김땡이</button>
+              <button>김땡이</button>
+              <button>이땡이</button>
+            </div>
+          </div>
+          <div className="kds_modal_btn">
+            <button>닫기</button>
+            <button>확인</button>
+          </div>
+        </div> */}
       </div>
     </div>
   );
